6a8a068f7ab63266acf498fa8bf52562882e875d
Sedyshev S.M. <zxlive@yandex.ru>
2024-09-05 10:39:26 +0400

    1

--- game1.py
+++ game1.py
@@ -1,11 +1,30 @@
 # Simple pygame program
 
 # Import and initialize the pygame library
-import pygame.sprite
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
 pygame.init()
 
+# Define constants for the screen width and height
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+
+FPS = 60        # число кадров в секунду
+clock = pygame.time.Clock()
 
 # Run until the user asks to quit
 running = True
@@ -21,10 +40,14 @@ while running:
 
     # Draw a solid blue circle in the center
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-    pygame.draw.rect(screen, (255,0,0), 10, 2, 10)
+    # pygame.draw.rect(screen, (255,0,0), 10, 2, 10)
 
     # Flip the display
-    pygame.display.flip()
+    # pygame.display.flip()
+
+    pygame.display.update()
+    clock.tick(FPS)
+
 
 # Done! Time to quit.
 pygame.quit()
